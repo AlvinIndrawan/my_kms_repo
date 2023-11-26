6eaa96bc1037eaec00c119871e14ebdb547b246a
Alvin_Indrawan <alvin.av639@gmail.com>
2023-11-26 07:16:06 +0000

    Update UI register

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -15,187 +15,227 @@ class _RegisterState extends State<Register> {
       // appBar: AppBar(
       //   title: Text('Halaman StatefulWidget'),
       // ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 15),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Spacer(),
-            Center(
-              child: Text(
-                'Create Account',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-              ),
-            ),
-            Spacer(),
-            Row(
-              children: [
-                Text('Nama'),
-                Text(
-                  '*', // Add your mandatory icon (e.g., an asterisk)
-                  style: TextStyle(
-                    color: Colors.red,
-                  ),
-                )
-              ],
-            ),
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Nama',
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              children: [
-                Text('Jurusan'),
-                Text(
-                  '*', // Add your mandatory icon (e.g., an asterisk)
-                  style: TextStyle(
-                    color: Colors.red,
-                  ),
-                )
-              ],
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 10),
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.grey,
-                  width: 1.0,
-                  style: BorderStyle.solid,
+      body: ListView(
+        padding: EdgeInsets.all(15),
+        children: [
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+            'Create Account',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Row(
+            children: [
+              Text('Nama'),
+              Text(
+                '*', // Add your mandatory icon (e.g., an asterisk)
+                style: TextStyle(
+                  color: Colors.red,
                 ),
-                borderRadius: BorderRadius.circular(4.0),
+              )
+            ],
+          ),
+          TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Nama',
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              Text('Jurusan'),
+              Text(
+                '*', // Add your mandatory icon (e.g., an asterisk)
+                style: TextStyle(
+                  color: Colors.red,
+                ),
+              )
+            ],
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.grey,
+                width: 1.0,
+                style: BorderStyle.solid,
               ),
-              child: DropdownButton<String>(
-                value: selectedOptionJurusan,
-                onChanged: (newValue) {
-                  setState(() {
-                    selectedOptionJurusan = newValue!;
-                  });
-                },
-                items: ['Teknik Informatika', 'Sistem Informasi']
-                    .map((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-                isExpanded: true,
-                underline: Container(),
+              borderRadius: BorderRadius.circular(4.0),
+            ),
+            child: DropdownButton<String>(
+              value: selectedOptionJurusan,
+              onChanged: (newValue) {
+                setState(() {
+                  selectedOptionJurusan = newValue!;
+                });
+              },
+              items: ['Teknik Informatika', 'Sistem Informasi']
+                  .map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              isExpanded: true,
+              underline: Container(),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              Text('NIM/NIDN'),
+              Text(
+                '*', // Add your mandatory icon (e.g., an asterisk)
+                style: TextStyle(
+                  color: Colors.red,
+                ),
+              )
+            ],
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'NIM/NIDN',
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              Text('Email'),
+              Text(
+                '*', // Add your mandatory icon (e.g., an asterisk)
+                style: TextStyle(
+                  color: Colors.red,
+                ),
+              )
+            ],
+          ),
+          TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Email',
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              Text('No HP'),
+              Text(
+                '*', // Add your mandatory icon (e.g., an asterisk)
+                style: TextStyle(
+                  color: Colors.red,
+                ),
+              )
+            ],
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'No HP',
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              Text('Password'),
+              Text(
+                '*', // Add your mandatory icon (e.g., an asterisk)
+                style: TextStyle(
+                  color: Colors.red,
+                ),
+              )
+            ],
+          ),
+          TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Password',
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              Text('Confirm Password'),
+              Text(
+                '*', // Add your mandatory icon (e.g., an asterisk)
+                style: TextStyle(
+                  color: Colors.red,
+                ),
+              )
+            ],
+          ),
+          TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Confirm Password',
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: ElevatedButton(
+              onPressed: () {
+                // Insert the code you want to run when the button is pressed
+              },
+              child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                  child: Text('Create Account')),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
               ),
             ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              children: [
-                Text('NIM/NIDN'),
-                Text(
-                  '*', // Add your mandatory icon (e.g., an asterisk)
-                  style: TextStyle(
-                    color: Colors.red,
-                  ),
-                )
-              ],
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'NIM/NIDN',
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              children: [
-                Text('Password'),
-                Text(
-                  '*', // Add your mandatory icon (e.g., an asterisk)
-                  style: TextStyle(
-                    color: Colors.red,
-                  ),
-                )
-              ],
-            ),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Password',
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              children: [
-                Text('Confirm Password'),
-                Text(
-                  '*', // Add your mandatory icon (e.g., an asterisk)
-                  style: TextStyle(
-                    color: Colors.red,
-                  ),
-                )
-              ],
-            ),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Confirm Password',
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Spacer(),
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              child: ElevatedButton(
-                onPressed: () {
-                  // Insert the code you want to run when the button is pressed
-                },
-                child: Padding(
-                    padding: EdgeInsets.symmetric(vertical: 15),
-                    child: Text('Create Account')),
-                style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.black),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: OutlinedButton(
+              onPressed: () {
+                // Insert the code you want to run when the button is pressed
+              },
+              child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                  child: Text('Login')),
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+                side: MaterialStateProperty.all<BorderSide>(
+                  BorderSide(width: 1.0, color: Colors.black),
                 ),
               ),
             ),
-            SizedBox(
-              height: 10,
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              child: OutlinedButton(
-                onPressed: () {
-                  // Insert the code you want to run when the button is pressed
-                },
-                child: Padding(
-                    padding: EdgeInsets.symmetric(vertical: 15),
-                    child: Text('Login')),
-                style: ButtonStyle(
-                  foregroundColor:
-                      MaterialStateProperty.all<Color>(Colors.black),
-                  side: MaterialStateProperty.all<BorderSide>(
-                    BorderSide(width: 1.0, color: Colors.black),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-          ],
-        ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+        ],
       ),
     );
   }
